3fc832b9780516821d55ea87caa08ee573a91eac
Kevin Lau <kevinlau@live.com>
2013-08-20 18:11:49 -0400

    One last one

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ ThirdSampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contacts"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +13,7 @@ describe "StaticPages" do
 
   it "should have the right title" do
 	visit '/static_pages/home'
-	expect(page) .to have_title("Ruby on Rails Tutorial Sample App | Home")
+	expect(page) .to have_title("#{base_title} | Home")
 	end
 end
 
@@ -23,7 +26,7 @@ describe "Help page" do
 
 it "should have the right title" do
 	visit '/static_pages/help'
-	expect(page) .to have_title("Ruby on Rails Tutorial Sample App | Help")
+	expect(page) .to have_title("#{base_title} | Help")
 	end
 end
 
@@ -36,7 +39,19 @@ describe "About Page" do
 
 it "should have the right title" do
 	visit '/static_pages/about'
-	expect(page) .to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	expect(page) .to have_title("#{base_title} | About Us")
+	end
+end
+
+describe "Contact Us Page" do
+	it "should have the content 'Contact Us'" do
+		visit '/static_pages/contacts'
+		expect(page) .to have_content('Contact Us')
+	end
+
+	it "should have the right title" do
+		visit '/static_pages/contacts'
+		expect(page) .to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
 	end
 end
 
